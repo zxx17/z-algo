a41e1490855b18f2990408b86ec332eea84bd944
Xinxuan Zhuo <1575165952@qq.com>
2023-12-02 16:33:27 +0800

    init: LinkList Practice

--- step01/src/test/java/com/zhuo/base/TestOfLinkList.java
+++ step01/src/test/java/com/zhuo/base/TestOfLinkList.java
@@ -2,9 +2,7 @@ package com.zhuo.base;
 
 import com.zhuo.entity.ListNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Xinxuan Zhuo
@@ -19,7 +17,7 @@ public class TestOfLinkList {
 
     /**
      * 图书整理 I
-     * 反转链表
+     * 反转链表 递归
      */
     List<Integer> reverseList = new ArrayList<>();
 
@@ -43,6 +41,7 @@ public class TestOfLinkList {
 
     /**
      * 图书整理 I
+     * 反转链表 栈
      */
     public int[] reverseBookList2(ListNode head) {
         LinkedList<Integer> stack = new LinkedList<>();
@@ -60,6 +59,7 @@ public class TestOfLinkList {
 
     /**
      * 删除链表节点
+     * 双指针
      */
     public ListNode deleteNode(ListNode head, int val) {
         // 处理头节点为目标节点的情况
@@ -84,7 +84,7 @@ public class TestOfLinkList {
 
     /**
      * 训练计划 III
-     * 反转链表
+     * 反转链表 栈
      */
     public ListNode trainningPlan1(ListNode head) {
         if (head == null) {
@@ -107,6 +107,8 @@ public class TestOfLinkList {
 
     /**
      * 训练计划 III
+     * 反转链表
+     * 递归
      */
     public ListNode trainningPlan2(ListNode head) {
         return recur(head, null);
@@ -124,6 +126,7 @@ public class TestOfLinkList {
     /**
      * 删除链表节点
      * 训练计划 II
+     * 快慢指针
      */
     public ListNode trainingPlan(ListNode head, int cnt) {
         ListNode fast = head, slow = head;
@@ -141,4 +144,75 @@ public class TestOfLinkList {
     }
 
 
+    /**
+     * 合并两个有序链表
+     * 循环 排序
+     */
+    public ListNode trainningPlan(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        List<Integer> container = new ArrayList<>();
+        while (l1 != null) {
+            container.add(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            container.add(l2.val);
+            l2 = l2.next;
+        }
+        Collections.sort(container);
+        ListNode newHead = new ListNode(container.get(0));
+        ListNode cur = new ListNode();
+        cur = newHead;
+        for (int index = 1; index < container.size(); index++) {
+            ListNode node = new ListNode(container.get(index));
+            cur.next = node;
+            cur = node;
+        }
+        return newHead;
+    }
+
+    /**
+     * 合并两个有序链表
+     * 三指针
+     */
+    public ListNode trainningPlan2(ListNode l1, ListNode l2) {
+        ListNode npoint = new ListNode();
+        ListNode ncur = npoint;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                ncur.next = l1;
+                l1 = l1.next;
+            } else {
+                ncur.next = l2;
+                l2 = l2.next;
+            }
+            ncur = ncur.next;
+        }
+        ncur.next = l2 != null ? l2 : l1;
+        return npoint.next;
+    }
+
+    /**
+     * 合并两个有序链表
+     * 递归
+     */
+    public ListNode trainningPlan3(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }else if(l2 == null) {
+            return l1;
+        }else if(l1.val < l2.val) {
+            l1.next = trainningPlan3(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = trainningPlan3(l1, l2.next);
+            return l2;
+        }
+    }
+
+
 }
+
+
