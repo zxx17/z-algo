ec04ebfbaa75bf5ce57721d002fa7ea888baa1df
Xinxuan Zhuo <1575165952@qq.com>
2023-12-16 10:46:01 +0800

    add: leetcode stackandqueue q1

--- step01/src/test/java/com/zhuo/base/TestOfStackAndQueen.java
+++ step01/src/test/java/com/zhuo/base/TestOfStackAndQueen.java
@@ -287,6 +287,50 @@ public class TestOfStackAndQueen {
         }
     }
 
+    class MedianFinder {
+        private PriorityQueue<Integer> minHeap;
+        private PriorityQueue<Integer> maxHeap;
+
+        /** initialize your data structure here. */
+        public MedianFinder() {
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        }
+
+        public void addNum(int num) {
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.add(num);
+            } else {
+                minHeap.add(num);
+            }
+
+            // 平衡两个堆的大小
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.add(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+        }
+
+        public double findMedian(){
+            if (maxHeap.size() > minHeap.size()) {
+                return maxHeap.peek();
+            } else {
+                return (maxHeap.peek() + minHeap.peek()) / 2.0;
+            }
+        }
+    }
+
+    @Test
+    public void testMedianFinder(){
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(2);
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian());
+
+    }
+
+
 
 
 }
